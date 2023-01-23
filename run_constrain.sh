c3470d84f203fb2b84bd4861666b7c721cf796fc
heartsh <heartsh@heartsh.io>
2023-01-24 07:19:14 +0900

    Fix "run_constrain.sh"

--- run_constrain.sh
+++ run_constrain.sh
@@ -10,4 +10,4 @@ cd consprob-trained/scripts
 constrain -i $TRAIN_DATA -o $TRAIN_LOG
 sed -E -i "s/^(consprob-trained=)\"[0-9]+\.[0-9]+\"$/\1={path = \"\.\/consprob-trained\"}/" ./Cargo.toml
 cd ../..
-$ RUSTFLAGS='--emit asm -C target-feature=+avx -C target-feature=+ssse3 -C target-feature=+mmx' cargo install --path . -f
+RUSTFLAGS='--emit asm -C target-feature=+avx -C target-feature=+ssse3 -C target-feature=+mmx' cargo install --path . -f
